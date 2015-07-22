27dd39616b66a6b4d922b4f7fc20cf934bdf5748
Aaron Elvina <aelvina@gmail.com>
2015-07-21 19:32:12 -0600

    Basic makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(solve) i <<- solve
+    
+    getinverse <- function() i
+    
+    list(set = set, get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
